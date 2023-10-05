d8fc022b67a37697a2a2c3e6f35608f41877884d
voidchambersocial <145938983+voidchambersocial@users.noreply.github.com>
2023-10-05 23:01:00 +0300

    major
    same layout, big fixes, new background

--- Untitled-2.js
+++ Untitled-2.js
@@ -1,90 +1,65 @@
-let slideIndex = [1,1];
-let slideId = ["mySlides1", "mySlides2"]
-showSlides(1, 0);
-showSlides(1, 1);
 
-// JavaScript to show/hide the sticky button as you scroll
-window.addEventListener('scroll', function() {
-    var button = document.querySelector('.sticky-button');
-    if (window.scrollY > 100) {
-        button.style.display = 'block';
-    } else {
-        button.style.display = 'none';
-    }
-});
+let slideIndex = 1;
+showSlides(slideIndex);
 
-
-
-function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
-function showSlides(n, no) {
+function showSlides(n) {
   let i;
-  let x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}    
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
   }
-  x[slideIndex[no]-1].style.display = "block";  
-}// JavaScript Document
-
-document.getElementById("subscriptionForm").addEventListener("submit", function (e) {
-            e.preventDefault();
-            const email = document.querySelector("input[name='mail']").value;
-            
-            // Validate email address here (basic validation)
-            if (validateEmail(email)) {
-                // Save the email to local storage (example)
-                saveEmailToLocalStorage(email);
-                alert("Thank you for subscribing!");
-                // You can also send the email to an API or perform other actions here
-            } else {
-                alert("Please provide a valid email address.");
-            }
-        });
-
-        function validateEmail(email) {
-            // Basic email validation using a regular expression
-            const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-            return emailPattern.test(email);
-        }
-
-        function saveEmailToLocalStorage(email) {
-            // Example: Store the email in local storage
-            const existingEmails = JSON.parse(localStorage.getItem("subscribers")) || [];
-            existingEmails.push(email);
-            localStorage.setItem("subscribers", JSON.stringify(existingEmails));
-        }
-// Retrieve JSON data from local storage
-const storedData = localStorage.getItem("subscribers");
-
-// Check if there's any data in local storage
-if (storedData) {
-    // Parse the JSON data into a JavaScript object
-    const subscribers = JSON.parse(storedData);
-
-    // Create a string to hold the list of subscriber emails
-    let subscriberList = "";
-
-    // Loop through the subscribers and add each email to the list
-    subscribers.forEach((email, index) => {
-        // Add a comma and space after each email (except the last one)
-        if (index === subscribers.length - 1) {
-            subscriberList += email;
-        } else {
-            subscriberList += email + ", ";
-        }
-    });
-
-    // Insert the list into the HTML element with the id "subscriberList"
-    const subscriberListElement = document.getElementById("subscriberList");
-    if (subscriberListElement) {
-        subscriberListElement.textContent = "List of Subscribers: " + subscriberList;
-    } else {
-        console.log("HTML element with id 'subscriberList' not found.");
-    }
-} else {
-    console.log("No subscriber data found in local storage.");
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
 }
+
+
+
+
+//read more
+function readmore() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
+//read more
+function readmore1() {
+  var dots = document.getElementById("dots1");
+  var moreText = document.getElementById("more1");
+  var btnText = document.getElementById("myBtn1");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
+
+
+
+
