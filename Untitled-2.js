5a2e24748f8d9701c8e20a1baca04c4dbd3bbe8b
voidchambersocial <145938983+voidchambersocial@users.noreply.github.com>
2023-10-06 18:41:52 +0300

    flexboxes in flexcontainers in flexboxes in flex containers :)

--- Untitled-2.js
+++ Untitled-2.js
@@ -1,3 +1,7 @@
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello World";
+}
+
 
 let slideIndex = 1;
 showSlides(slideIndex);
@@ -9,17 +13,17 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
+  let clicker = document.getElementsByClassName("clicker");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  for (i = 0; i < clicker.length; i++) {
+    clicker[i].className = clicker[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  clicker[slideIndex-1].className += " active";
 }
 
 
